c15c3b425868b120b2e9c9f88ca9a8b3a40f7242
aliraahim <ali.raahim@gmail.com>
2017-04-22 18:10:17 +0500

    new UI

--- index.php
+++ index.php
@@ -5,14 +5,14 @@ $states = array();
 $states_data=file($filename);
 foreach($states_data as $state)
 {
-   $states[] = trim($state);
+    $states[] = trim($state);
 }
 $filename = 'gs://simplehomecontrolauto.appspot.com/health.txt';
 $health = array();
 $health_data=file($filename);
 foreach($health_data as $health_point)
 {
-   $health[] = trim($health_point);
+    $health[] = trim($health_point);
 }
 
 $filename = 'gs://simplehomecontrolauto.appspot.com/schedule.txt';
@@ -34,69 +34,195 @@ foreach($new_states_data as $new_state)
     <!-- Bootstrap -->
     <link rel="shortcut icon" href="favicon.ico?v=1" type="image/x-icon">
     <?php include ('stylesheets.php'); ?>
+
 </head>
-<body class = "exp">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-            <?php if ((time() - $health[0] < 20) || (debug)){
-                echo '<div class = "switch" id = "switch1" value='.$states[0].'>';
-                if ($states[0] == "on") {
-                    echo'<img class = "toggle active" src = "assets/bulb-on.jpg" width = "300px" value = "on"/>';
-                    echo'<img class = "toggle inactive" src = "assets/bulb-off.jpg" width = "300px" value = "off"/></div>';
-                } elseif ($states[0] == "off") {
-                    echo'<img class = "toggle inactive" src = "assets/bulb-on.jpg" width = "300px" value = "on"/>';
-                    echo'<img class = "toggle active" src = "assets/bulb-off.jpg" width = "300px" value = "off"/></div>';
-                }} else {
+<body class = "cards">
+<br>
+<br>
+<br>
+<div id="timer" class="overlay">
+
+    <!-- Button to close the overlay navigation -->
+    <a href="javascript:void(0)" class="closebtn" onclick="closeTimer()">&times;</a>
+
+    <!-- Overlay content -->
+    <div class="overlay-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <form action = "" method = "" class="form-inline timerForm">
+<!--                        <label class="" for="inlineFormCustomSelect">Turn</label>-->
+                        <span class="labels">Turn</span>
+                        <select class="selectpicker" id="new_state" name = "new_state" data-width="auto">
+                            <option value="off">off</option>
+                            <option value="on">on</option>
+                        </select>
+                        <br>
+                        <span class="labels">after</span>
+                        <input type="number" class="form-control" name="number" id = "number" min="1" max="2000" placeholder="5" required>
+                        <select class="selectpicker" name = "units" id="units" data-width="auto">
+                            <option value="minutes">minutes</option>
+                            <option value="hours">hours</option>
+                        </select>
+                        </br>
+                        </br>
+                        <button type="submit" class="btn btn-primary timer-btn">Set timer</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
+<div class="container">
+    <div class="row toprow">
+        <!--<div class="col-xs-6 col-lg-offset-2 col-lg-4 mycard">-->
+        <!--http://shoelace.io/#830fe4793d41c811597b2576c0abfedf-->
+        <div class="col-sm-offset-3 col-sm-6">
+            <!-- The overlay -->
+
+
+            <!-- Use any element to open/show the overlay navigation menu -->
+            <div class="card">
+                <?php if ((time() - $health[0] < 20) || (debug)){
+                    echo '<div class = "switch" id = "switch1" value='.$states[0].'>';
+                    if ($states[0] == "on") {
+                        echo'<img class = "toggle active" src = "assets/new-bulb-on.jpg" style="width:300px" value = "on"/>';
+                        echo'<img class = "toggle inactive" src = "assets/new-bulb-off.jpg" style="width:300px" value = "off"/></div>';
+                    } elseif ($states[0] == "off") {
+                        echo'<img class = "toggle inactive" src = "assets/new-bulb-on.jpg" style="width:300px" value = "on"/>';
+                        echo'<img class = "toggle active" src = "assets/new-bulb-off.jpg" style="width:300px" value = "off"/></div>';
+                    }} else {
                     echo '<h1 style = "font-size: 7em;">&#9785;</h1>';
                     echo '<h2>Chotu is not working!</h2>';
                     echo '<a href =".">Check again</a>';
                 }
                 ?>
             </div>
-        </div> 
-        <div class = "row">
-            <div class = "col-sx-12 timer">
-                <?php if ($new_states[0] > time()){
-                date_default_timezone_set("Asia/Karachi");
-                $new_state = explode('-',$new_states[0]);
-                echo 'Will turn ' . $new_state[1] . ' at: ' . date("F j, Y, g:i a", $new_state[0]);}?>
-                <form action = "timer.php" method = "POST" class="form-inline">
-                    <label class="" for="inlineFormCustomSelect">Turn</label>
-                    <select class="selectpicker" id="inlineFormCustomSelect" name = "new_state" data-width="auto">
-                        <option value="off">off</option>
-                        <option value="on">on</option>
-                    </select>
-                    <label class="" for="number">after</label>
-                    <input type="number" class="form-control" name="number" id = "inlineFormInput" min="1" max="2000" placeholder="5" required>
-                    <select class="selectpicker" name = "units" id="inlineFormCustomSelect" data-width="auto">
-                        <option value="minutes">minutes</option>
-                        <option value="hours">hours</option>
-                    </select>
-                    </br>
-                    </br>
-                    <button type="submit" class="btn btn-primary">Set timer</button>
-                </form>
+        </div>
+        <!--<div class="col-xs-6 col-lg-4 mycard">-->
+    </div>
+    <div class="row">
+        <!--<div class="col-xs-6 col-lg-offset-2 col-lg-4 mycard">-->
+        <div class="col-xs-6 col-sm-offset-3 col-sm-3 cardColumn">
+            <div class ="card" onclick="openTimer()">
+                <div class ="cardImage">
+                    <img src="assets/timer.png">
+                </div>
+                <div class="container">
+                    <h4><b>Timer</b></h4>
+                    <p>Auto on/off.</p>
+                </div>
             </div>
+
+        </div>
+        <!--<div class="col-xs-6 col-lg-4 mycard">-->
+        <div class="col-xs-6 col-sm-3 cardColumn">
+            <div class ="card" onclick="openSettings()">
+                <div class ="card" onclick="openSettings()">
+                    <div class ="cardImage">
+                        <img src="assets/settings.png">
+                    </div>
+                    <div class="container">
+                        <h4><b>Settings</b></h4>
+                        <p>Tap to modify.</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
-  <?php include('javascript.php'); ?>  
-  <script>
-      $('.switch').click(function() {
+</div>
+<?php include('javascript.php'); ?>
+<script>
+    $('.switch').click(function() {
         var switches = $(this).find('.toggle');
-        switches.toggleClass('inactive');  
-        switches.toggleClass('active'); 
+        switches.toggleClass('inactive');
+        switches.toggleClass('active');
         $(this).attr('value',$(this).find('.active').attr('value'));
-    switches.attr('disabled',true);
-    var url = 'homeauto.php' + '?' + $(this).attr('id') + '=' + $(this).attr('value');
-    $.ajax({
-                url: url,
-                type: 'get',
-            }).done(function(data) {
-                switches.attr('disabled',false);
-            });
-});
-  </script>
+        switches.attr('disabled',true);
+        var url = 'homeauto.php' + '?' + $(this).attr('id') + '=' + $(this).attr('value');
+        $.ajax({
+            url: url,
+            type: 'get',
+        }).done(function(data) {
+            switches.attr('disabled',false);
+        });
+    });
+</script>
+<script>
+    function timerButtonRestore() {
+        var text = "Set timer";
+        $('.timer-btn').text(text);
+        $('.timer-btn').css('background-color','#337ab7');
+        $('.timer-btn').disabled = '';
+
+    }
+
+    /* Open when someone clicks on the span element */
+    function openTimer() {
+        document.getElementById("timer").style.width = "100%";
+    }
+
+    /* Close when someone clicks on the "x" symbol inside the overlay */
+    function closeTimer() {
+        document.getElementById("timer").style.width = "0%";
+        timerButtonRestore();
+    }
+</script>
+<script>
+    $(document).ready(function() {
+
+        // process the form
+        $('form').submit(function(event) {
+
+            // get the form data
+            // there are many ways to get this data using jQuery (you can use the class or id also)
+            var formData = {
+                'new_state': $('#new_state').val(),
+                'number'   : $('#number').val(),
+                'units'    : $('#units').val()
+            };
+
+            // process the form
+            $.ajax({
+                type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+                url         : 'timer.php', // the url where we want to POST
+                data        : formData, // our data object
+                dataType    : 'json', // what type of data do we expect back from the server
+                encode          : true
+            })
+            // using the done promise callback
+                .done(function(data) {
+
+                    // log data to the console so we can see
+                    if (data.success == true){
+                        var number = $('#number').val();
+                        var units = $('#units').val();
+                        if (number == 1){
+                            units = units.slice(0, -1);
+                        }
+
+                        var text = "Timer set for " + number + " " + units + "!";
+                        $('.timer-btn').text(text);
+                        $('.timer-btn').css('background-color','green');
+                        $('.timer-btn').disabled = 'disabled';
+                    } else {
+                        alert('Sadness');
+                    }
+
+
+                    // here we will handle errors and validation messages
+                });
+
+            // stop the form from submitting the normal way and refreshing the page
+            event.preventDefault();
+        });
+
+    });
+</script>
 
 </body>
 </html>
--- timer.php
+++ timer.php
@@ -2,6 +2,7 @@
 //writes the schedule file with timestamp provided by user
 $filename = 'gs://simplehomecontrolauto.appspot.com/schedule.txt';
 $new_states = array();
+$success = false;
 
 $new_states_data=file($filename);
 foreach($new_states_data as $new_state)
@@ -20,6 +21,7 @@ if (isset($_POST['units']) && isset($_POST['number']) && isset($_POST['new_state
     $action = $_POST['new_state'];
     $new_time = 60 - $new_time % 60 + $new_time;
     $new_states[0] = $new_time.'-'.$action;
+    $success = true;
 }
 $file = fopen($filename, 'w');
 
@@ -27,5 +29,8 @@ foreach ($new_states as $index => $new_state){
     fwrite($file, $new_state);
 }
 fclose($file);
-echo json_encode($new_states[0]);
+
+$resp = ["success" => $success, "scheduled_states" => $new_states];
+echo json_encode($resp);
+
 ?>
